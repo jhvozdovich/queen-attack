c700dba602758bc2899f4d214b26d55ada45d1da
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-04 11:55:19 -0700

    add logic for queen attack validation

--- queen.cs
+++ queen.cs
@@ -4,6 +4,47 @@ class QueenAttack
 {
   static void Main()
   {
-    
+    Console.WriteLine("Enter Queen X coordinate:");
+    int queenX = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter Queen Y coordinate:");
+    int queenY = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter Foe X coordinate:");
+    int foeX = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter Foe Y coordinate:");
+    int foeY = int.Parse(Console.ReadLine());
+
+    if (queenX == foeX || queenY == foeY || (Math.Abs(queenX - foeX) == Math.Abs(queenY - foeY)))
+    {
+      Console.WriteLine("Attack is valid!");
+    }
+    else
+    {
+      Console.WriteLine("Noep.");
+    }
   }
 }
+
+
+
+
+/* TEST BOARD
+
+  - - - - -
+  - - - T - 1, 3
+  - - - - -
+  - Q - - - 3, 1
+  - - - - -
+
+  T - - - - 0, 0
+  - Q - - -
+  - - - - -
+  - - - - -
+  - - - - - 1,1
+  
+  - - T - - 0, 2
+  - Q - - - 1, 1
+  - - - - -
+  - - - - -
+  - - - - - 
+
+*/
